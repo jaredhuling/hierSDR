d3eb048f5efadd586cfd7337ab1526f5a1992d35
jaredhuling <huling.7@osu.edu>
2018-01-28 15:03:39 -0500

    made functions explicitly for optimization

--- R/newton_constrained.R
+++ R/newton_constrained.R
@@ -295,6 +295,151 @@ createAboveList <- function(combin.mat)
 }
 
 
+opt.est.eqn <- function(init, est.eqn, est.eqn.grad,
+                        opt.method = c("lbfgs.x", "bfgs", "lbfgs2",
+                                       "bfgs.x",
+                                       "lbfgs",
+                                       "spg", "ucminf"),
+                        nn = 0.75,
+                        optimize.nn = FALSE, maxit = 100, verbose = FALSE)
+{
+    opt.method <- match.arg(opt.method)
+
+    if (optimize.nn)
+    {
+        init.par <- c(log(nn / (1 - nn)), init)
+    } else
+    {
+        init.par <- init
+    }
+
+    if (opt.method == "bfgs")
+    {
+        slver <-   optim(par     = init.par, # beta.init[(d+1):nrow(beta.init),],
+                         fn      = est.eqn,
+                         gr      = est.eqn.grad,
+                         method  = "BFGS",
+                         control = list(maxit = maxit, abstol = 1e-10))
+        if (optimize.nn) slver$par <- slver$par[-1]
+    } else if (opt.method == "lbfgs")
+    {
+        slver <-   optim(par     = init.par,
+                         fn      = est.eqn,
+                         gr      = est.eqn.grad,
+                         method  = "L-BFGS",
+                         control = list(maxit = maxit, factr = 1e-10))
+        if (optimize.nn) slver$par <- slver$par[-1]
+    } else if (opt.method == "lbfgs2")
+    {
+        slver <-   lbfgs(vars      = init.par,
+                         call_eval = est.eqn,
+                         call_gra  = est.eqn.grad,
+                         max_iterations = maxit)
+        if (optimize.nn) slver$par <- slver$par[-1]
+    } else if (opt.method == "bfgs.x")
+    {
+        slver <-   optimx(par     = init.par,
+                          fn      = est.eqn,
+                          #gr      = est.eqn.grad,
+                          control = list(trace = 1 * verbose,
+                                         maxit = maxit,
+                                         kkt = FALSE),
+                          method  = "BFGS")
+        if (is.null(slver$par) & names(slver)[1] == "p1")
+        {
+            if (optimize.nn)
+            {
+                if (verbose) print(paste("optimized nn:", exp(slver$p1) / (1 + exp(slver$p1))))
+                slver <- list(par = unlist(slver[2:length(init.par)]),
+                              value = slver$value,
+                              convcode = slver$convcode)
+            } else
+            {
+                slver <- list(par = unlist(slver[1:length(init.par)]),
+                              value = slver$value,
+                              convcode = slver$convcode)
+            }
+        }
+    } else if (opt.method == "lbfgs.x")
+    {
+
+        slver <-   optimx(par     = init.par, # c(log(nn / (1 - nn)), init.par),
+                          fn      = est.eqn,
+                          #gr      = est.eqn.grad,
+                          control = list(trace = 1 * verbose,
+                                         maxit = maxit,
+                                         kkt = FALSE),
+                          method  = "L-BFGS-B")
+        if (is.null(slver$par) & names(slver)[1] == "p1")
+        {
+            if (optimize.nn)
+            {
+                if (verbose) print(paste("optimized nn:", exp(slver$p1) / (1 + exp(slver$p1))))
+                slver <- list(par = unlist(slver[2:length(init.par)]),
+                              value = slver$value,
+                              convcode = slver$convcode)
+            } else
+            {
+                slver <- list(par = unlist(slver[1:length(init.par)]),
+                              value = slver$value,
+                              convcode = slver$convcode)
+            }
+        }
+    } else
+    {
+        slver <-   optimx(par     = init.par,
+                          fn      = est.eqn,
+                          #gr      = est.eqn.grad,
+                          control = list(trace = 1 * verbose,
+                                         maxit = maxit,
+                                         kkt = FALSE),
+                          method  = opt.method)
+        if (is.null(slver$par) & names(slver)[1] == "p1")
+        {
+            if (optimize.nn)
+            {
+                if (verbose) print(paste("optimized nn:", exp(slver$p1) / (1 + exp(slver$p1))))
+                slver <- list(par = unlist(slver[2:length(init.par)]),
+                              value = slver$value,
+                              convcode = slver$convcode)
+            } else
+            {
+                slver <- list(par = unlist(slver[1:length(init.par)]),
+                              value = slver$value,
+                              convcode = slver$convcode)
+            }
+        }
+    }
+    slver
+}
+
+try.nn <- function(nn.vals = c(0.1, 0.25, 0.5, 0.75, 0.9, 0.95),
+                   init, est.eqn, est.eqn.grad,
+                   opt.method = c("lbfgs.x", "bfgs", "lbfgs2",
+                                  "bfgs.x",
+                                  "lbfgs",
+                                  "spg", "ucminf"),
+                   nn = 0.75,
+                   optimize.nn = FALSE, maxit = 10, verbose = FALSE)
+{
+    n.try  <- length(nn.vals)
+    values <- numeric(n.try)
+    for (i in 1:n.try)
+    {
+        slver.cur <- opt.est.eqn(init         = init,
+                                 est.eqn      = est.eqn,
+                                 est.eqn.grad = est.eqn.grad,
+                                 opt.method   = opt.method,
+                                 nn           = nn,
+                                 optimize.nn  = optimize.nn,
+                                 maxit        = maxit,
+                                 verbose      = verbose)
+
+        values[i] <- slver.cur$value
+    }
+    nn.vals[which.min(values)]
+}
+
 
 semi.phd.hier.newton <- function(x.list, y, d = rep(1L, 3L),
                                  maxit = 10L, h = NULL, B = NULL, vic = FALSE,
@@ -637,118 +782,28 @@ semi.phd.hier.newton <- function(x.list, y, d = rep(1L, 3L),
     #                 fn = est.eqn, maxit = maxit, tol = 1e-8,
     #                 verbose = verbose)
 
-    if (optimize.nn)
-    {
-        init.par <- c(log(nn / (1 - nn)), unlist(beta.list.init))
-    } else
-    {
-        init.par <- unlist(beta.list.init)
-    }
+    init <- unlist(beta.list.init)
 
-    if (opt.method == "bfgs")
-    {
-        slver <-   optim(par     = init.par, # beta.init[(d+1):nrow(beta.init),],
-                         fn      = est.eqn,
-                         gr      = est.eqn.grad,
-                         method  = "BFGS",
-                         control = list(maxit = maxit, abstol = 1e-10))
-        if (optimize.nn) slver$par <- slver$par[-1]
-    } else if (opt.method == "lbfgs")
-    {
-        slver <-   optim(par     = init.par,
-                         fn      = est.eqn,
-                         gr      = est.eqn.grad,
-                         method  = "L-BFGS",
-                         control = list(maxit = maxit, factr = 1e-10))
-        if (optimize.nn) slver$par <- slver$par[-1]
-    } else if (opt.method == "lbfgs2")
-    {
-        slver <-   lbfgs(vars      = init.par,
-                         call_eval = est.eqn,
-                         call_gra  = est.eqn.grad,
-                         max_iterations = maxit)
-        if (optimize.nn) slver$par <- slver$par[-1]
-    } else if (opt.method == "bfgs.x")
-    {
-        slver <-   optimx(par     = init.par,
-                          fn      = est.eqn,
-                          #gr      = est.eqn.grad,
-                          control = list(trace = 1 * verbose,
-                                         maxit = maxit,
-                                         kkt = FALSE),
-                          method  = "BFGS")
-        if (is.null(slver$par) & names(slver)[1] == "p1")
-        {
-            if (optimize.nn)
-            {
-                if (verbose) print(paste("optimized nn:", exp(slver$p1) / (1 + exp(slver$p1))))
-                slver <- list(par = unlist(slver[2:length(init.par)]),
-                              value = slver$value,
-                              convcode = slver$convcode)
-            } else
-            {
-                slver <- list(par = unlist(slver[1:length(init.par)]),
-                              value = slver$value,
-                              convcode = slver$convcode)
-            }
-        }
-    } else if (opt.method == "lbfgs.x")
-    {
+    nn <- try.nn(nn.vals      = c(0.1, 0.25, 0.5, 0.75, 0.9, 0.95),
+                 init         = init,
+                 est.eqn      = est.eqn,
+                 est.eqn.grad = est.eqn.grad,
+                 opt.method   = opt.method,
+                 nn           = nn,
+                 optimize.nn  = optimize.nn,
+                 maxit        = 10L,
+                 verbose      = verbose)
+    print(paste("best nn:", nn))
 
-        if (optimize.nn)
-        {
-            init.par <- c(log(nn / (1 - nn)), unlist(beta.list.init))
-        } else
-        {
-            init.par <- unlist(beta.list.init)
-        }
-        slver <-   optimx(par     = init.par, # c(log(nn / (1 - nn)), init.par),
-                          fn      = est.eqn,
-                          #gr      = est.eqn.grad,
-                          control = list(trace = 1 * verbose,
-                                         maxit = maxit,
-                                         kkt = FALSE),
-                          method  = "L-BFGS-B")
-        if (is.null(slver$par) & names(slver)[1] == "p1")
-        {
-            if (optimize.nn)
-            {
-                if (verbose) print(paste("optimized nn:", exp(slver$p1) / (1 + exp(slver$p1))))
-                slver <- list(par = unlist(slver[2:length(init.par)]),
-                              value = slver$value,
-                              convcode = slver$convcode)
-            } else
-            {
-                slver <- list(par = unlist(slver[1:length(init.par)]),
-                              value = slver$value,
-                              convcode = slver$convcode)
-            }
-        }
-    } else
-    {
-        slver <-   optimx(par     = init.par,
-                          fn      = est.eqn,
-                          #gr      = est.eqn.grad,
-                          control = list(trace = 1 * verbose,
-                                         maxit = maxit,
-                                         kkt = FALSE),
-                          method  = opt.method)
-        if (is.null(slver$par) & names(slver)[1] == "p1")
-        {
-            if (optimize.nn)
-            {
-                if (verbose) print(paste("optimized nn:", exp(slver$p1) / (1 + exp(slver$p1))))
-                slver <- list(par = unlist(slver[2:length(init.par)]),
-                              value = slver$value,
-                              convcode = slver$convcode)
-            } else
-            {
-                slver <- list(par = unlist(slver[1:length(init.par)]),
-                              value = slver$value,
-                              convcode = slver$convcode)
-            }
-        }
-    }
+    slver <- opt.est.eqn(init         = init,
+                         est.eqn      = est.eqn,
+                         est.eqn.grad = est.eqn.grad,
+                         opt.method   = opt.method,
+                         nn           = nn,
+                         optimize.nn  = optimize.nn,
+                         maxit        = maxit,
+                         verbose      = verbose)
+
 
     if (verbose)
     {
@@ -826,6 +881,7 @@ semi.phd.hier.newton <- function(x.list, y, d = rep(1L, 3L),
     list(beta = beta.mat.list, beta.init = beta, solver.obj = slver,
          #beta.rand.init = t(t(beta.rand.init) %*% sqrt.inv.cov),
          cov = cov, sqrt.inv.cov = sqrt.inv.cov,
+         nn  = nn,
          vic.est.eqn = vic.eqn, vic.eqns = vic.eqns,
          vic = vic, vic2 = vic2,
          sse = sse.vec, mse = mse.vec)
