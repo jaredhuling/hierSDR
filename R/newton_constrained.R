f4282bf76de355dab6d8c841c26a47edff358d72
jaredhuling <huling.7@osu.edu>
2018-01-28 20:27:33 -0500

    default change

--- R/newton_constrained.R
+++ R/newton_constrained.R
@@ -647,7 +647,7 @@ semi.phd.hier.newton <- function(x.list, y, d = rep(1L, 3L),
     # test which nn values minimize the most effectively
     if (is.null(nn))
     {
-        nn <- try.nn(nn.vals      = c(0.1, 0.25, 0.5, 0.75, 0.9, 0.95),
+        nn <- try.nn(nn.vals      = c(0.15, 0.25, 0.5, 0.75, 0.9, 0.95),
                      init         = init,
                      est.eqn      = est.eqn,
                      est.eqn.grad = est.eqn.grad,
